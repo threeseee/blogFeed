47ad44c462f44cc80397389221712c52eecc8077
Brittney Johnson <bjohns@mit.edu>
2014-08-01 23:46:40 -0400

    Updated bios

--- bios.php
+++ bios.php
@@ -65,71 +65,128 @@
                 <h2 class="page-header">We're so cool</h2>
             </div>
             <div class="col-lg-4 col-sm-6 text-center">
-                <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
+                <img class="img-circle img-responsive img-center" style="width:200px" src="https://scontent-b-mia.xx.fbcdn.net/hphotos-xfa1/t1.0-9/26391_1113067564923_1385568_n.jpg" alt="">
                 <h3>Hojin
                     <small>Have you read his blog cuz it's gr8</small>
                 </h3>
                 <p>Dude's been coding and taking pretty pictures and living in TDC. He was gonna do MEET but it got cancelled so now they're doing a week of remote teaching. </p>
             </div>
             <div class="col-lg-4 col-sm-6 text-center">
-                <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
+                <img class="img-circle img-responsive img-center" style="width:200px;height:200px" src="https://fbcdn-sphotos-g-a.akamaihd.net/hphotos-ak-xap1/t1.0-9/73497_10200140434013241_1736958195_n.jpg" alt="">
                 <h3>Annie
                     <small>So Smart And Pretty</small>
                 </h3>
                 <p>Livin' in NYC taking pretty pictures too and coding smart things. Also just bein' a fab lady in general because Annie.</p>
             </div>
             <div class="col-lg-4 col-sm-6 text-center">
-                <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
+                <img class="img-circle img-responsive img-center" style="width:200px;height:200px" src="https://fbcdn-sphotos-d-a.akamaihd.net/hphotos-ak-xpf1/v/t1.0-9/5563_10201343678817805_752782533_n.jpg?oh=06d14dd5d24be82ca332e1bcc76f7eb4&oe=5440088C&__gda__=1414258485_c42e7d1dc4a652b65a4180a07db2461c" alt="">
                 <h3>Ariella
                     <small>Always has stash of chocolate</small>
                 </h3>
-                <p>Was in Israel with MISTI until it was way to dangerous. Now she's back in NJ, the garden state. Also she has a pretty smile. </p>
+                <p>Was in Israel with MISTI until it was way too dangerous. Now she's back in NJ, the garden state. Also she has a pretty smile. </p>
             </div>
             <div class="col-lg-4 col-sm-6 text-center">
-                <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
+                <img class="img-circle img-responsive img-center" style="width:200px;height:200px" src="https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-xaf1/v/t1.0-9/230506_1039184070798_4434_n.jpg?oh=8745d288541d398762d91c988f624216&oe=5455E6B8&__gda__=1413902832_b599cb081229952eeb6b57c56426e852" alt="">
                 <h3>Anthony
                     <small>World Changer</small>
                 </h3>
                 <p>Dude's in Chile for the summer working at a really legit civil engineering company where he speaks Spanish all day, cuz he's cool like that. </p>
             </div>
             <div class="col-lg-4 col-sm-6 text-center">
-                <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
+                <img class="img-circle img-responsive img-center" style="width:200px;height:200px" src="https://scontent-a-mia.xx.fbcdn.net/hphotos-xaf1/t1.0-9/205727_1022982821654_2390_n.jpg" alt="">
                 <h3>Cathleen
                     <small>No pants are best pants</small>
                 </h3>
                 <p>NYC stuff coding for a little company that has gotten littler recently. Made a really cool app thing for study buddies! So talented. </p>
             </div>
             <div class="col-lg-4 col-sm-6 text-center">
-                <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
+                <img class="img-circle img-responsive img-center" style="width:200px;height:200px" src="https://fbcdn-sphotos-g-a.akamaihd.net/hphotos-ak-xfa1/t31.0-8/1655359_10152635729808942_1990593586_o.jpg" alt="">
                 <h3>Brittney
                     <small>wtf am i doing</small>
                 </h3>
                 <p>Living in Chile with Anthony working for a startup. Since the startup ran out of work to do after week 3, has been mostly fucking around on youtube and tumblr and/or trying to code. </p>
             </div>
             <div class="col-lg-4 col-sm-6 text-center">
-                <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
+                <img class="img-circle img-responsive img-center" style="width:200px;height:200px" src="http://www.howtoquitsmokingweedblog.com/wp-content/uploads/2012/12/What_Happens_When_You_Stop_Smoking_Weed.jpeg" alt="">
                 <h3>Jorge
                     <small>The Physics Dude</small>
                 </h3>
-                <p>TDC etc. </p>
+                <p>Livin' la vida loca in TDC. Has UROP in fancy fiziks thing, does coding for physicsy thingies idk mystery. </p>
             </div>
             <div class="col-lg-4 col-sm-6 text-center">
-                <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
+                <img class="img-circle img-responsive img-center" style="width:200px;height:200px" src="http://a3.files.biography.com/image/upload/c_fill,g_face,h_300,q_80,w_300/MTE1ODA0OTcxNzgzMzkwNzMz.jpg" alt="">
                 <h3>Vivian
                     <small>The Writer</small>
                 </h3>
-                <p>DAAD shit I'm out of time sorry </p>
+                <p>In Germany for much of summer, but traveled a ton. Her program is called DAAD probably, since that's the name of the blog, but it's probably through MISTI. </p>
             </div>
             <div class="col-lg-4 col-sm-6 text-center">
-                <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
-                <h3>Someone
-                    <small>Something</small>
+                <img class="img-circle img-responsive img-center" style="width:200px;height:200px" src="https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-xap1/v/t1.0-9/35803_405219944901_590027_n.jpg?oh=f623e3ba4cb23d19e17ee9a45c2473a2&oe=5445DBA4&__gda__=1414335873_b4c29a9fdd05f309e7af0d93155b4d75" alt="">
+                <h3>Fran
+                    <small>Lights, camera... actually just lights</small>
                 </h3>
-                <p>Out of time ahh </p>
+                <p>Spends time working at VECNA, where ueses the robot that throws coffee mugs to fight off UPOP recruiters probably. Also hangs with Nico lots. (sexy time shhh)</p>
             </div>
+            <div class="col-lg-4 col-sm-6 text-center">
+                <img class="img-circle img-responsive img-center" style="width:200px;height:200px" src="https://fbcdn-sphotos-c-a.akamaihd.net/hphotos-ak-xaf1/t31.0-8/265333_177317332328339_3702053_o.jpg" alt="">
+                <h3>David
+                    <small>The tall black one</small>
+                </h3>
+                <p>What is David doing? We don't know. probably involves Smash Bros, looking for Ness, and/or making more cool table things. </p>
+            </div>
+            <div class="col-lg-4 col-sm-6 text-center">
+                <img class="img-circle img-responsive img-center" style="width:200px;height:200px" src="https://fbcdn-sphotos-e-a.akamaihd.net/hphotos-ak-xfp1/v/t1.0-9/34577_407478963422_4936342_n.jpg?oh=1a10027cb8b4fa07c89bafba5858ab08&oe=543732DA&__gda__=1413386274_f49fe081e6e1055fef0acb8429f9ee1f" alt="">
+                <h3>Nico
+                    <small>Karate kid</small>
+                </h3>
+                <p>Taught for MITES and tried to avoid getting in trouble for having Fran over 24/7. Now has gone home because MITES is over? </p>
+            </div>
+            <div class="col-lg-4 col-sm-6 text-center">
+                <img class="img-circle img-responsive img-center" style="width:200px;height:200px" src="http://marquez.mit.edu/images/MeStata.jpg" alt="">
+                <h3>Elliott
+                    <small>Two L's, Two T's</small>
+                </h3>
+                <p>Microsoft. He's bingin'. He's bingin' out in Seattle. Go use bing, he gets paid more. Probably. 16% chance of raise. Ish. </p>
+            </div>
+            <div class="col-lg-4 col-sm-6 text-center">
+                <img class="img-circle img-responsive img-center" style="width:200px;height:200px" src="https://fbcdn-sphotos-b-a.akamaihd.net/hphotos-ak-xfa1/v/t1.0-9/199239_1939453251916_4504815_n.jpg?oh=6ab0ffef2fbb1a290741fe2cb9d5087c&oe=54369DAB&__gda__=1413365312_2e9d0ead2d8677c1e77d4cb9a7426c01" alt="">
+                <h3>Steven
+                    <small>Our newbie</small>
+                </h3>
+                <p>Found Ness. Beyond this, who knows? </p>
+            </div>
+             <div class="col-lg-4 col-sm-6 text-center">
+                <img class="img-circle img-responsive img-center" style="width:200px;height:200px" src="https://fbcdn-sphotos-d-a.akamaihd.net/hphotos-ak-xap1/t1.0-9/1918111_1287043379829_7857272_n.jpg" alt="">
+                <h3>Andre
+                    <small>Fabitch. Fab + bitch. It works.</small>
+                </h3>
+                <p> At VECNA with Fran. Probably codes and does things. I dunno. Mystery. </p>
+            </div>
+             <div class="col-lg-4 col-sm-6 text-center">
+                <img class="img-circle img-responsive img-center" style="width:200px;height:200px" src="https://scontent-b-mia.xx.fbcdn.net/hphotos-xap1/t1.0-9/p417x417/1016088_10151849297259017_146468900_n.jpg" alt="">
+                <h3>Paul
+                    <small>????</small>
+                </h3>
+                <p>I literally have zero idea what Paul's life is right now. </p>
+            </div>
+        </div>
+        <div class="col-lg-4 col-sm-6 text-center">
+                <img class="img-circle img-responsive img-center" style="width:200px;height:200px" src="https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-xap1/v/t1.0-9/67121_10151427773418942_1163187764_n.jpg?oh=64ebc362590bfb5c1eaa0087fff7bdb4&oe=543CF99D&__gda__=1412865716_bb06e2da7c8cc6d4896bab4886d3327a" alt="">
+                <h3>Natalie
+                    <small>Sexy Beast</small>
+                </h3>
+                <p>This girl is a tiny package of sunshine and sexy. Go Hoosiers! She's chillin at WILG and working at that medical place in Cambridge. Also spends time with boy!</p>
+            </div>
+        </div>
+        <hr>
+        <div class="text-center">
+        <h3> Honorable Mentions</h3>
+            <p>Juan, Eva, Sergio, Rebecca, and Winta. HMU if you want a bio! </p>
         </div>
 
         <hr>
+        
+
     
     <!-- JavaScript -->
     <script src="../js/jquery-1.10.2.js"></script>
